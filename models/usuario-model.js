65eac2e358ff7d993560b4d011a96ea29502f4a8
AnthonyCallaRoman <anthonycallaroman@gmail.com>
2021-10-04 09:46:13 -0500

    ultimo

--- models/usuario-model.js
+++ models/usuario-model.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const Usuario = new Schema({
-    nombre: {type: String, required: true, trim: true},
-    email: {type: String, required: true, trim: true},
-    password: {},
-    
+    nombre: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true },
+    password: { type:String, required:true, trim: true },
 })
 
 module.exports = mongoose.model('usuario',Usuario)
